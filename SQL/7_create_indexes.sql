f6a4cab8d3736ea4c1c902215bf41821531409fe
Shantanu TK <46672146+binaryBodhi@users.noreply.github.com>
2026-06-04 17:33:17 +0530

    Update 7_create_indexes.sql

--- SQL/7_create_indexes.sql
+++ SQL/7_create_indexes.sql
@@ -3,3 +3,4 @@ CREATE INDEX idx_account_customer ON Accounts(CustomerID);
 CREATE INDEX idx_transaction_account ON Transactions(AccountID);
 CREATE INDEX idx_loan_customer ON Loans(CustomerID);
 CREATE INDEX idx_card_customer ON Cards(CustomerID);
+CREATE INDEX idx_card_customer ON Cards(CustomerID);
